54c4570ea113096d79172842a1bd931c7d895cd3
Arian Rezazadeh <notarian.rez@gmail.com>
2025-03-16 10:53:04 +0330

    refactor: use sudo instead of relying on wg-quick root password prompt

--- electro-rs/src/wireguard.rs
+++ electro-rs/src/wireguard.rs
@@ -13,12 +13,12 @@ pub fn is_interface_up(if_name: &str) -> anyhow::Result<bool> {
 pub fn connect(config_path: &str) -> anyhow::Result<()> {
     // if path doesn't exit with .conf it will look up /etc/wireguard
     assert!(config_path.ends_with(".conf"));
-    run_command("wg-quick", &["up", config_path]).map(|_| ())
+    run_command("sudo", &["wg-quick", "up", config_path]).map(|_| ())
 }
 
 pub fn disconnect(config_path: &str) -> anyhow::Result<()> {
     assert!(config_path.ends_with(".conf"));
-    run_command("wg-quick", &["down", config_path]).map(|_| ())
+    run_command("sudo", &["wg-quick", "down", config_path]).map(|_| ())
 }
 
 fn run_command(command: &str, args: &[&str]) -> anyhow::Result<String> {
